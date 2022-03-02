171222d0b178c9074fc6f92e6513f7db270ba7b7
Justin Hagerty <wheelsmanx@gmail.com>
2022-03-01 22:26:14 -0500

    added target_processor again

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import modules.target_processor as target_processor
+import modules.my_logger as my_logger
+import json
 
-
-
+my_logger.config_file("./logs/app_target_logging.log", "debug")
---
+++ modules/target_processor/__init__.py
@@ -0,0 +1,12 @@
+import modules.my_logger as my_logger
+import json
+
+
+def start():
+    return True
+
+def check_status():
+    return True
+
+def kill_all_targets():
+    return True
