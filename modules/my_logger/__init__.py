8a027459e45229b4158c769a8de6f8f679c21da6
Justin Hagerty <wheelsmanx@gmail.com>
2022-03-06 13:34:17 -0500

    moved the requests over and added file handlers

--- modules/my_logger/__init__.py
+++ modules/my_logger/__init__.py
@@ -4,10 +4,16 @@ from datetime import datetime
 
 def config_file(config_file_location, log_level):
     try:
+
         if log_level == "debug":
             log_level = logging.DEBUG
+
         if log_level == "info":
             log_level = logging.INFO
+
+        if log_level == "error":
+            log_level = logging.ERROR
+
         logging.basicConfig(format='[%(asctime)s]%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename=config_file_location, level=log_level)
     except Exception as error:
         print(f'[{datetime.now()}][ERROR] LOGGER-ERROR {error}')
--- modules/target_processor/__init__.py
+++ modules/target_processor/__init__.py
@@ -2,9 +2,14 @@ import modules.my_logger as my_logger
 import json
 
 
+def run_threads():
+    return True
+
 def start():
     return True
 
+def check_target_folder_for_next_target():
+    return True
 def check_status():
     return True
 
